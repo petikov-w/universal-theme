64d54a23dcf7de99bb04cc441f988a75e42e9c00
Petikov Vladimir <vx7xv@inbox.ru>
2020-10-06 01:23:19 +0300

    Добавлены файлы header.php, footer.php, сформирован каталог assets

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
+
+function enqueue_universal_style() {
+	wp_enqueue_style( 'style', get_stylesheet_uri());
+	wp_enqueue_style( 'universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css','style', null, null);
+}
+
+// отключаем строку для вызова админ панели вверху сайта
+add_filter('show_admin_bar', '__return_false');
+
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 get_header();
+?>
+<h1>Привет, мир!</h1>
+<?php
 get_footer();
