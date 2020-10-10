8c30db398860fa40b2ee7d2627f24cf90ab744ce
Petikov Vladimir <vx7xv@inbox.ru>
2020-10-10 18:29:24 +0300

    Сверстана левая часть блока hero

---
+++ front-page.php
@@ -0,0 +1,29 @@
+<?php
+get_header();
+?>
+
+<div class="container">
+	<div class="hero">
+		<div class="left">
+            <img src="<?php echo get_template_directory_uri().'/assets/images/image.jpg'?>" alt="" class="post-thumb">
+			<a href="#" class="author">
+				<img src="<?php echo get_template_directory_uri().'/assets/images/avatar.png' ?>" alt="" class="avatar">
+                <div class="author-bio">
+                    <span class="author-name">Имя автора</span>
+                    <span class="author-rank">Должность</span>
+                </div>
+			</a><!-- /.author -->
+            <div class="post-text">
+                <a href="#" class="category-name">Рубрика</a>
+                <h2 class="post-title">Название поста Название поста Название поста Название поста</h2>
+                <a href="#" class="more">Читать далее</a>
+            </div>
+		</div><!-- /.left -->
+		<div class="right">
+
+		</div><!-- /.right -->
+	</div><!-- /.hero -->
+</div><!-- /.container -->
+
+<?php
+get_footer();
--- header.php
+++ header.php
@@ -2,7 +2,8 @@
 <html <?php language_attributes();?>>
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--	<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
 	<title>Universal</title>
 	<?php wp_head();?>
 </head>
