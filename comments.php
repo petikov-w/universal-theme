d3a97a7df3eef74d14095c1414d2b46a1c66829b
Petikov Vladimir <vx7xv@inbox.ru>
2020-11-16 01:01:59 +0300

    Продолжение работы с комментариями - 4

--- comments.php
+++ comments.php
@@ -20,7 +20,7 @@
 // Своя функция для вывода отдельного поста
 
 function universal_theme_comment( $comment, $args, $depth ) {
-    // Проверка в каком стиле у нас родитель (ol, ul или div)
+	// Проверка в каком стиле у нас родитель (ol, ul или div)
 	if ( 'div' === $args['style'] ) {
 		$tag       = 'div';
 		$add_below = 'comment';
@@ -32,25 +32,25 @@ function universal_theme_comment( $comment, $args, $depth ) {
 	$classes = ' ' . comment_class( empty( $args['has_children'] ) ? '' : 'parent', null, null, false );
 	?>
 
-	<<?php echo $tag, $classes; ?> id="comment-<?php comment_ID() ?>">
+    <<?php echo $tag, $classes; ?> id="comment-<?php comment_ID() ?>">
 	<?php if ( 'div' != $args['style'] ) { ?>
-		<div id="div-comment-<?php comment_ID() ?>" class="comment-body"><?php
+        <div id="div-comment-<?php comment_ID() ?>" class="comment-body"><?php
 	} ?>
     <div class="comment-author-avatar">
-        <?php
-            if ( $args['avatar_size'] != 0 ) {
-                echo get_avatar( $comment, $args['avatar_size'] );
-            }
-        ?>
+		<?php
+		if ( $args['avatar_size'] != 0 ) {
+			echo get_avatar( $comment, $args['avatar_size'] );
+		}
+		?>
     </div>
     <div class="comment-content">
         <div class="comment-author vcard">
-		    <?php
-		    printf(
-			    __( '<cite class="comment-author-name">%s</cite>' ),
-			    get_comment_author_link()
-		    );
-		    ?>
+			<?php
+			printf(
+				__( '<cite class="comment-author-name">%s</cite>' ),
+				get_comment_author_link()
+			);
+			?>
             <span class="comment-meta commentmetadata">
 		<a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>">
 			<?php
@@ -67,13 +67,13 @@ function universal_theme_comment( $comment, $args, $depth ) {
 
         </div>
 
-	    <?php if ( $comment->comment_approved == '0' ) { ?>
+		<?php if ( $comment->comment_approved == '0' ) { ?>
             <em class="comment-awaiting-moderation">
-			    <?php _e( 'Ваш комментарий ожидает модерации.' ); ?>
+				<?php _e( 'Ваш комментарий ожидает модерации.' ); ?>
             </em><br/>
-	    <?php } ?>
+		<?php } ?>
 
-	    <?php comment_text(); ?>
+		<?php comment_text(); ?>
 
         <div class="reply">
             <svg width="14" height="14" class="icon comment-silver-icon">
@@ -81,24 +81,24 @@ function universal_theme_comment( $comment, $args, $depth ) {
                              <?php echo get_template_directory_uri().'/assets/images/sprite-like.svg#comment'?>">
                 </use>
             </svg>&nbsp
-		    <?php
-		    comment_reply_link(
-			    array_merge(
-				    $args,
-				    array(
-					    'add_below' => $add_below,
-					    'depth'     => $depth,
-					    'max_depth' => $args['max_depth']
-				    )
-			    )
-		    ); ?>
+			<?php
+			comment_reply_link(
+				array_merge(
+					$args,
+					array(
+						'add_below' => $add_below,
+						'depth'     => $depth,
+						'max_depth' => $args['max_depth']
+					)
+				)
+			); ?>
         </div>
 
     </div>
 
 
 	<?php if ( 'div' != $args['style'] ) { ?>
-		</div>
+        </div>
 	<?php }
 }
 
@@ -114,39 +114,40 @@ if ( post_password_required() ) {
 ?>
 
 <div id="comments" class="comments-area">
-<!--    ===============================================================-->
+    <!--    ===============================================================-->
 	<?php
 	// Проверка на наличие комментариев к посту
 	if ( have_comments() ) :
 		?>
-    <div class="commments-header">
-        <h2 class="comments-title">
-		    <?php echo 'Комментарии  ' . '<span class="comment-count">' . get_comments_number() . '</span>' ?>
-        </h2><!-- .comments-title -->
-        <a href="#" class="comments-add-button">
-            <svg width="18" height="18" class="icon comment-silver-icon">
-                <use xlink:href="
+        <div class="commments-header">
+            <h2 class="comments-title">
+				<?php echo 'Комментарии  ' . '<span class="comment-count">' . get_comments_number() . '</span>' ?>
+            </h2><!-- .comments-title -->
+            <a href="#" class="comments-add-button">
+                <svg width="18" height="18" class="icon comment-silver-icon">
+                    <use xlink:href="
                              <?php echo get_template_directory_uri().'/assets/images/sprite.svg#pencil'?>">
-                </use>
-            </svg>&nbsp Добавить комментарий</a>
-    </div>
+                    </use>
+                </svg>&nbsp Добавить комментарий</a>
+        </div>
 
-<!--    ===============================================================-->
+        <!--    ===============================================================-->
 
 
 
 
 		<?php the_comments_navigation(); ?>
-<!--Выводим список комментариев-->
+        <!--Выводим список комментариев-->
         <ol class="comments-list">
-<!--    Выводим отдельные комментарии-->
+            <!--    Выводим отдельные комментарии-->
 			<?php
 			wp_list_comments(
 				array(
 					'style'      => 'ol',
 					'short_ping' => true,
-                    'avatar_size'=> 75,
-                    'callback' => 'universal_theme_comment'
+					'avatar_size'=> 75,
+					'callback' => 'universal_theme_comment',
+					'login_text' => 'Зарегистрируйтесь, если хотите прокомментировать'
 				)
 			);
 			?>
@@ -164,7 +165,19 @@ if ( post_password_required() ) {
 
 	endif; // Check for have_comments().
 
-	comment_form();
+	comment_form(
+		[
+			'label_submit' => 'Отправить',
+			'title_reply' => '',
+			// переопределим textarea (тело формы)
+			'comment_field' => '<div class="comment-form-comment"><label for="comment" class="comment-label">' .
+			                   _x( 'Что Вы думаете на этот счет?', 'noun' ) . '</label><br /> 
+                               <div class="comment-wrapper">'. get_avatar(get_current_user_id(),75) . '                                  
+                               <textarea id="comment" name="comment" aria-required="true" class="comment-textarea"></textarea>
+                                </div></div>',
+			'logged_in_as'         => '',
+		]
+	);
 	?>
 
 </div><!-- #comments -->
