0706f4adac3cab33fd042057b87c99edcc2f1c13
Petikov Vladimir <vx7xv@inbox.ru>
2020-11-28 00:55:54 +0300

    Начата работа с таксономиями к ДЗ 37

--- functions.php
+++ functions.php
@@ -202,3 +202,60 @@ function plural_form($number, $after) {
 	$cases = array (2, 0, 1, 1, 1, 2);
 	echo $number.' '.$after[ ($number%100>4 && $number%100<20)? 2: $cases[min($number%10, 5)] ];
 }
+
+//==================================================================
+
+
+
+function add_new_taxonomies() {
+	/* создаем функцию с произвольным именем и вставляем
+	в неё register_taxonomy() */
+	register_taxonomy('teacher',
+		array('lesson'),
+		array(
+			'hierarchical' => false,
+			/* true - по типу рубрик, false - по типу меток,
+			по умолчанию - false */
+			'labels' => array(
+				/* ярлыки, нужные при создании UI, можете
+				не писать ничего, тогда будут использованы
+				ярлыки по умолчанию */
+				'name' => 'Преподователи',
+				'singular_name' => 'Преподаватель',
+				'search_items' =>  'Найти преподавателя',
+				'all_items' => 'Все преподаватели',
+				'edit_item' => 'Редактировать преподавателя',
+				'update_item' => 'Изменить преподавателя',
+				'add_new_item' => 'Добавить преподавателя',
+				'new_item_name' => 'Название нового преподавателя',
+				'separate_items_with_commas' => 'Разделяйте платформы запятыми',
+				'add_or_remove_items' => 'Добавить или удалить платформу',
+				'choose_from_most_used' => 'Выбрать из наиболее часто используемых платформ',
+				'menu_name' => 'Преподаватели'
+			),
+			'show_admin_column' => true,
+			'public' => true,
+			/* каждый может использовать таксономию, либо
+			только администраторы, по умолчанию - true */
+			'show_in_nav_menus' => true,
+			/* добавить на страницу создания меню */
+			'show_ui' => true,
+			/* добавить интерфейс создания и редактирования */
+			'show_tagcloud' => false,
+			/* нужно ли разрешить облако тегов для этой таксономии */
+			'update_count_callback' => '_update_post_term_count',
+			/* callback-функция для обновления счетчика $object_type */
+			'query_var' => true,
+			/* разрешено ли использование query_var, также можно
+			указать строку, которая будет использоваться в качестве
+			него, по умолчанию - имя таксономии */
+			'rewrite' => array(
+				/* настройки URL пермалинков */
+				'slug' => 'teacher', // ярлык
+				'hierarchical' => false // разрешить вложенность
+
+			),
+		)
+	);
+}
+add_action( 'init', 'add_new_taxonomies', 0 );
